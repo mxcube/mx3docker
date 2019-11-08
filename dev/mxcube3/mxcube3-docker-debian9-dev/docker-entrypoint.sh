b670a44a09f08a7b473fc59aad0920eec76674f6
Laís Pessine do Carmo <lais.carmo@lnls.br>
2019-11-08 14:41:40 -0300

    Call mxcube-server with python3

--- dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
+++ dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
@@ -9,6 +9,6 @@ source "$NVM_DIR/bash_completion"
 vncserver :1 -geometry 1680x1050 -depth 24 &
 
 redis-server&
-python mxcube3-server -r test/HardwareObjectsMockup.xml --log-file mxcube.log&
+python3 mxcube3-server -r test/HardwareObjectsMockup.xml --log-file mxcube.log&
 npm start&
 wait
